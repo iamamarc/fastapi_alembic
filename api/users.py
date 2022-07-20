c734bc97627da1186700bb69a11032096c1fda40
Mark Nzei <nzeimark@gmail.com>
2022-07-20 11:12:52 +0100

    done

--- api/users.py
+++ api/users.py
@@ -2,16 +2,21 @@ from typing import List
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from db.db_setup import get_db
+from api.utils.courses import get_user_courses
+from db.db_setup import get_db, async_get_db
 from api.utils.users import get_user, get_users, get_user_by_email, create_user
+from schemas.course import Course
 from schemas.user import User, UserCreate
 
 router = APIRouter()
 
 
 @router.get("/users", response_model=List[User])
-async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+async def read_users(
+    skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
+):
     users = get_users(db, skip=skip, limit=limit)
     return users
 
@@ -20,13 +25,29 @@ async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_
 async def create_new_user(user: UserCreate, db: Session = Depends(get_db)):
     db_user = get_user_by_email(db=db, email=user.email)
     if db_user:
-        raise HTTPException(status_code=400, detail="Email is already registered")
+        raise HTTPException(
+            status_code=400, detail="Email is already registered"
+        )
     return create_user(db=db, user=user)
 
 
+# @router.get("/users/{user_id}", response_model=User)
+# async def read_user(user_id: int, db: Session = Depends(get_db)):
+#     db_user = get_user(db=db, user_id=user_id)
+#     if db_user is None:
+#         raise HTTPException(status_code=404, detail="User not found")
+#     return db_user
+
+
 @router.get("/users/{user_id}", response_model=User)
-async def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = get_user(db=db, user_id=user_id)
+async def read_user(user_id: int, db: AsyncSession = Depends(async_get_db)):
+    db_user = await get_user(db=db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
+
+
+@router.get("/users/{user_id}/courses", response_model=List[Course])
+async def read_user_courses(user_id: int, db: Session = Depends(get_db)):
+    courses = get_user_courses(user_id=user_id, db=db)
+    return courses
